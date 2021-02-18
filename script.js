9dde26043492a6242bc1e173a1d358bb1e0dd30d
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-17 18:20:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
- require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
+require([
+  "esri/config",
+  "esri/Map", 
+  "esri/views/MapView",
+  
+  "esri/widgets/BasemapToggle",
+  "esri/widgets/BasemapGallery"
+
+], function (esriConfig, Map, MapView, BasemapToggle, BasemapGallery) {
+
+esriConfig.apiKey =  "AAPKdc25ad7513a24c46b92cd024b53471173ysex4xHWQwBw2IQ-kNnn5MYzM2Maqnuc7YbUYeHblfF461bYrN1hiCHw4m9SK0E"; 
+
+  const map = new Map({
+          basemap: "arcgis-topographic" // Basemap layer service
         });
 
-        var view = new MapView({
-          container: "viewDiv",
+const view = new MapView({
+         container: "viewDiv", // Div element 
           map: map,
-          zoom: 10,
-          center: [-90, 38.85] // longitude, latitude
+          center: [-118.805, 34.027], // Longitude, latitude
+          zoom: 13, // Zoom level
         });
+  
+  const basemapToggle = new BasemapToggle({
+        view: view,
+        nextBasemap: "arcgis-imagery"
+     });
+
+  view.ui.add(basemapToggle,"bottom-right");
+  
+  const basemapGallery = new BasemapGallery({
+        view: view,
+        source: {
+          query: {
+            title: '"World Basemaps for Developers" AND owner:esri'
+          }
+        }
       });
+  
+  view.ui.add(basemapGallery, "top-right"); // Add to the view
+  
+ });
